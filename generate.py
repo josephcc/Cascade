f1a7e0280e7b24c53d7bf5fb424b0b2644b2c4dd
josephcc <josephcc@cmu.edu>
2014-11-22 15:27:47 -0500

    Output inset outset medata to dataset/ in generate.py

--- generate.py
+++ generate.py
@@ -1,11 +1,12 @@
 import sys
+from operator import *
 from random import shuffle
 
 from utils import *
 
 question_id = int(sys.argv[1])
 batch_size = 8
-init_set_size = 32
+inset_size = 32
 
 #cid, source_id, option_id, text, created_at, updated_at, worker_id, begin, end, creator_id, html, dimension_id, answer_id, answer_used, conductor_sent, question_id, img, video, video_id, vbegin, vend, clip_tag, vote_count, overlap, turker_option_id
 data = []
@@ -18,9 +19,9 @@ for line in dataset(question_id):
 for _ in range(5):
     shuffle(data)
 
-data = data[:init_set_size]
+inset, outset = data[:inset_size], data[inset_size:]
 
-for batch_id, batch in enumerate(batches(data, batch_size)):
+for batch_id, batch in enumerate(batches(inset, batch_size)):
     form = ''
     for cid, text in batch:
         form += render('_generate_form', {'clip_id': cid, 'text': text})
@@ -28,4 +29,10 @@ for batch_id, batch in enumerate(batches(data, batch_size)):
     print >> fp, render('generate', {'question': question(102), 'form': form})
     fp.close()
     
+inset = map(itemgetter(0), inset)
+outset = map(itemgetter(0), outset)
+
+fp = open('dataset/%s.metadata' % question_id, 'w')
+print >> fp, {'inset': inset, 'outset': outset}
+fp.close()
 
